2c952ef642f341e2cb8feabc3ffc372ffcf06718
Srinjani Roy Chowdhury <roysrinjani04@gmail.com>
2025-10-14 23:50:59 +0530

    feat: Add error handling middleware

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
 
 import { PORT } from './config/env.js';
 
@@ -6,13 +7,20 @@ import authRouter from './routes/auth.routes.js';
 import userRouter from './routes/user.routes.js';
 import subscriptionRouter from './routes/subscription.route.js';
 import connectToDatabase from './database/mondodb.js';
+import errorMiddleware from './middlewares/error.middleware.js';
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 // api/v1/auth/sign-up
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/subscriptions', subscriptionRouter);
+
+app.use(errorMiddleware);
 
 app.get('/', (req, res) => {
     res.send('Welcome to the Subscription Tracker API!');
